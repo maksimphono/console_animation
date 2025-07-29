bdeaa605fee6e905d53de8c33e112f51773f9f00
maksimphono <trukhinmaksimof@mail.ru>
2025-07-29 20:36:24 +0000

    feat: created basic configuration for dev container ; wrote initial code for TUI

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>    // For std::string
+#include <ncurses.h> // Include the ncurses library
+#include <vector>    // For std::vector (to store lines of text)
+#include <chrono>    // For std::chrono::seconds, std::chrono::milliseconds
+#include <thread>
+#include <cstdint>
+
+using namespace std;
+
+void mainloop() {
+    initscr();
+    noecho(); // turn off automatic echoing of typed characters
+    cbreak(); // make characters available to the program as soon as they are typed
+    keypad(stdscr, TRUE); // enable the interpretation of special keys
+    nodelay(stdscr, TRUE); // make getch() a non-blocking call
+    clear();
+
+    mvprintw(0, 0, "First line");
+    refresh();
+
+    int ch;
+    uint32_t i = 0;
+
+    while (true) {
+        ch = getch();
+
+        if (ch == 'q' || ch == 'Q') {
+            // If 'q' or 'Q' is pressed, break out of the loop to quit
+            break;
+        }
+        
+        clear(); // Clear the screen before redrawing
+        //move(current_line_idx + 2, current_col_idx);
+
+        refresh();
+        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // sleep for 500 milliseconds
+    }
+
+    endwin(); // cleanup
+}
+
+int main() {
+    try {
+        mainloop();
+    } catch (const exception& exp) {
+        cerr << exp.what() << endl;
+        return 1;
+    }
+
+    return 0;
+}
