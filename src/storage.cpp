c8bc9917e36c3e1bec566f42fa666a74197f5288
maksimphono <trukhinmaksimof@mail.ru>
2025-08-06 18:30:01 +0000

    created logic to write arguments and frames into the file

--- src/storage.cpp
+++ src/storage.cpp
@@ -4,24 +4,42 @@ namespace storage_ns {
     class StorageWriter : public ofstream {
     public:
         StorageWriter(string name) : ofstream(create_path(name)) {
-            this->open(create_path(name), fstream::out);
+            //this->open(create_path(name), fstream::out);
         }
         ~StorageWriter() {
             this->close();
         }
+        template <typename T = string>
+        uint32_t write_entry(const char* name, T value) {
+            *this << name << "=" << value << ";";
+            return 1;
+        }
+
         uint32_t write_metadata(EnvArguments& arguments) {
             // returns number of items written
-            cout << this->is_open();
-            return 0;
-            //if (!this->is_open()) return 0;
-            //this->seekp(0);
-            //StorageWriter& _this = *this;
+            uint32_t n = 0;
+            if (!this->is_open()) return 0;
+            this->seekp(0);
 
-            //_this << "INPUT_PATH=" << arguments.path << ";";
-            //_this << "FPS=" << arguments.fps;
+            n += this->write_entry("INPUT_PATH", arguments.path);
+            n += this->write_entry<int>("FPS", (int)arguments.fps);
+            n += this->write_entry("SIZE", format("{0}x{1}", arguments.size[0], arguments.size[1]));
+            n += this->write_entry("TIME", format("{0}-{1}", arguments.time[0], arguments.time[1]));
+
+            *this << endl;
+
+            return n;
         }
         uint32_t write_frames(vector<Frame>& frames) {
             // returns number of frames written
+            uint32_t n = 0;
+
+            for (auto& frame : frames) {
+                *this << frame.body;
+                n++;
+            }
+
+            return n;
         }
     };
 
@@ -44,11 +62,9 @@ namespace storage_ns {
     }
 
     void save_file(string name, vector<Frame>& frames, EnvArguments& arguments) {
-        //string n = "qwert";
-        //cout << create_path(n);
         StorageWriter writer(name);
 
         writer.write_metadata(arguments);
-        //file.write_frames(frames);
+        writer.write_frames(frames);
     }
 }
