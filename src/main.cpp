c67bdaee99941b4b6019c31eabb96d6a099ec027
maksimphono <trukhinmaksimof@mail.ru>
2026-05-09 00:18:51 +0800

    opt: changed files permissions

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,3 @@
-/*
-#include <iostream>
-#include "utils/exec_command.cpp"
-
-using namespace std;
-using namespace exec_command_ns;
-
-typedef int Pipe;
-
-class C{
-public:
-    string s;
-    C(string& s) {
-        this->s = s;
-    }
-};
-
-int main() {
-    CommandExecutor ff("ffmpeg -loglevel -8 -ss %d:%d:%d.000 -i %s -frames:v 1 -f image2pipe -", 512);
-    CommandExecutor jp("jp2a - --size=%dx%d", 512);
-
-    //cout << ff.command_templates.at(ff.command_templates.size() - 1).tmplt << endl;
-
-    //string s = ff.exec<string>(-9, 0, 0, 2, "/home/Rick.mp4");
-    //int p = ff.exec<int>(-9, 0, 0, 2, "/home/Rick.mp4");
-    int p = ff.exec<int>(-9, 0, 0, 2, "/home/build/Rick Astle Never Gonna give you up (Full version).mp4");
-    string s = jp.exec<string>(p, 70, 30);
-
-    cout << s;
-    return 0;
-}
-*/
-
 #include <iostream>
 #include <string>
 #include <vector>
